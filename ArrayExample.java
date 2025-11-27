7534d754df9412e948ce3e24ae6b1544a2375f6e
Deepalakshmi <146326704+DeepalakshmiElavarasan@users.noreply.github.com>
2025-11-27 22:25:00 +0530

    Add ArrayExample class to demonstrate array usage

---
+++ ArrayExample.java
@@ -0,0 +1,8 @@
+public class ArrayExample {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        System.out.println("First element: " + numbers[0]);
+        System.out.println("Last element: " + numbers[numbers.length - 1]);
+    }
+}
